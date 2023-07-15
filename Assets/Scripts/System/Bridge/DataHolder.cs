b1a6ea7fcaf37eabbd544b7a42f63f5d908e5852
Akira Watanabe <a0323w@gmail.com>
2023-07-16 01:33:51 +0900

    [update] キャラクターの参照を取得

--- Assets/Scripts/System/Bridge/DataHolder.cs
+++ Assets/Scripts/System/Bridge/DataHolder.cs
@@ -1,5 +1,18 @@
 public class DataHolder
 {
+    public CharacterBase Player { get; private set; }
+    public CharacterBase Opponent { get; private set; }
+
+    public void GetPlayer(CharacterBase character)
+    {
+        Player = character;
+    }
+
+    public void GetOpponent(CharacterBase character)
+    {
+        Opponent = character;
+    }
+
     public void GetData()
     {
 
